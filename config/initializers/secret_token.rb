0b3bad3eee1a89c8c612b8147d93c6779cb97cdc
Zach Taylor <taylozac85@gmail.com>
2013-02-21 13:23:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hummingbird::Application.config.secret_token = '4cc199d9f737978e2abd415c055bb3d5292b5a9607b432e3fdfe6ea604e19c3658e4dfca4734a26eaf0e625e39a7f5636270c77ade230112b92bb28192ce51fc'
